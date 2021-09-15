dcab1142e4a6c964a1c2156d684984fb671edcad
bunnybunbun37204 <stonksfarm37204@gmail.com>
2021-09-15 17:10:21 +0700

    update

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,13 +1,14 @@
 module.exports = (client, message, queue) => {
-        // const channel = client.channels.get(8);
-        // const delayTimeMs = 1 * 60000;
-        // channel.join().then(connection => {
-        //     // Yay, it worked!
-        //     console.log("Successfully connected.");
-        //   }).catch(e => {
-        //     // Oh no, it errored! Let's log it to console :)
-        //     console.error(e);
-        //   });
+        const id = message.guild.me.voice.channel.id;
+        const channel = client.channels.cache.get(id.toString());
+        const delayTimeMs = 1 * 60000;
+        channel.join().then(connection => {
+            // Yay, it worked!
+            console.log("Successfully connected.");
+          }).catch(e => {
+            // Oh no, it errored! Let's log it to console :)
+            console.error(e);
+          });
         console.log(message.guild.me.voice.channel.id);
         message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
 };
