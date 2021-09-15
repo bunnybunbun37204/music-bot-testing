ec4d6293653056c5eba300664af17f69ca130386
bunnybunbun37204 <stonksfarm37204@gmail.com>
2021-09-15 12:46:26 +0700

    illbefuckup

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,8 +1,5 @@
 const delayInMilliseconds = 15*60000;
 
 module.exports = (client, message, queue) => {
-    setTimeout(function () {
-        //add your code here to execute
-        message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
-    }, delayInMilliseconds);
+    message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
 };
