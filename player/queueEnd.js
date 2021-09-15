6cff951387c17e70cd6ec0c1f2fa6b860691de55
bunnybunbun37204 <stonksfarm37204@gmail.com>
2021-09-15 17:22:19 +0700

    maybe i did it

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -3,14 +3,15 @@ module.exports = (client, message, queue) => {
     const channel = client.channels.cache.get(id.toString());
     const delayTimeMs = 1 * 60000;
     setTimeout(function () {
-        channel.join().then(connection => {
-            // Yay, it worked!
-            console.log("Successfully connected.");
-        }).catch(e => {
-            // Oh no, it errored! Let's log it to console :)
-            console.error(e);
-        });
+        message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
     }, delayTimeMs);
+    channel.join().then(connection => {
+        // Yay, it worked!
+        console.log("Successfully connected.");
+    }).catch(e => {
+        // Oh no, it errored! Let's log it to console :)
+        console.error(e);
+    });
     console.log(message.guild.me.voice.channel.id);
-    message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
+
 };
