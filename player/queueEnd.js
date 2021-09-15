7120ff33835a54f62e6b3c8819c9e1297a1103c4
bunnybunbun37204 <stonksfarm37204@gmail.com>
2021-09-15 16:50:53 +0700

    waiting bot v2

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,3 +1,12 @@
 module.exports = (client, message, queue) => {
+        const channel = client.channel.get(message.guild.me.voice.channel.id);
+        const delayTimeMs = 1 * 60000;
+        channel.join().then(connection => {
+            // Yay, it worked!
+            console.log("Successfully connected.");
+          }).catch(e => {
+            // Oh no, it errored! Let's log it to console :)
+            console.error(e);
+          });
         message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
 };
