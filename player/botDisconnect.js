287e77ecd46d68e209034c0ef12738ab389ce555
bunnybunbun37204 <stonksfarm37204@gmail.com>
2021-09-15 12:38:46 +0700

    add delay time

--- player/botDisconnect.js
+++ player/botDisconnect.js
@@ -1,3 +1,8 @@
+const delayInMilliseconds = 15*60000;
+
 module.exports = (client, message, queue) => {
-    message.channel.send(`${client.emotes.error} - Music stopped as i have been disconnected from the channel !`);
+    setTimeout(function () {
+        //add your code here to execute
+        message.channel.send(`${client.emotes.error} - Music stopped as i have been disconnected from the channel !`);
+    }, delayInMilliseconds);
 };
