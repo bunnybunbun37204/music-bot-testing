0503b7caeb65c975fbdc66bc0359749fc31ab83f
bunnybunbun37204 <stonksfarm37204@gmail.com>
2021-09-15 17:18:05 +0700

    fuckup

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,14 +1,16 @@
-module.exports = async (client, message, queue) => {
-        const id = message.guild.me.voice.channel.id;
-        const channel = client.channels.cache.get(id.toString());
-        const delayTimeMs = 1 * 60000;
-       await channel.join().then(connection => {
+module.exports = (client, message, queue) => {
+    const id = message.guild.me.voice.channel.id;
+    const channel = client.channels.cache.get(id.toString());
+    const delayTimeMs = 1 * 60000;
+    setTimeout(function () {
+        channel.join().then(connection => {
             // Yay, it worked!
             console.log("Successfully connected.");
-          }).catch(e => {
+        }).catch(e => {
             // Oh no, it errored! Let's log it to console :)
             console.error(e);
-          });
-        console.log(message.guild.me.voice.channel.id);
-        message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
+        });
+    }, delayTimeMs);
+    console.log(message.guild.me.voice.channel.id);
+    message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
 };
