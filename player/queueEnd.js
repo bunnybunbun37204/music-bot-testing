e8c1405ec5ee6a29e5e45c558ac8128eb804d5f2
bunnybunbun37204 <stonksfarm37204@gmail.com>
2021-09-15 16:44:09 +0700

    bugs fixed

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,10 +1,3 @@
-const delayTimesMillisec = 1*60000;
-
 module.exports = (client, message, queue) => {
-    if (!client.player.getQueue(message)) {
-        setTimeout(function () {
-            client.player.pause(message);
-        }, delayTimesMillisec);
         message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
-    }
 };
