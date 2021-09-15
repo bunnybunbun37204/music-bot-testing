ab336bd4fabd2917933f69ee1ae690932148ac4e
bunnybunbun37204 <stonksfarm37204@gmail.com>
2021-09-15 12:43:56 +0700

    update delay

--- player/botDisconnect.js
+++ player/botDisconnect.js
@@ -1,8 +1,3 @@
-const delayInMilliseconds = 15*60000;
-
 module.exports = (client, message, queue) => {
-    setTimeout(function () {
-        //add your code here to execute
-        message.channel.send(`${client.emotes.error} - Music stopped as i have been disconnected from the channel !`);
-    }, delayInMilliseconds);
+    message.channel.send(`${client.emotes.error} - Music stopped as i have been disconnected from the channel !`);
 };
--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,3 +1,8 @@
+const delayInMilliseconds = 15*60000;
+
 module.exports = (client, message, queue) => {
-    message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
+    setTimeout(function () {
+        //add your code here to execute
+        message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
+    }, delayInMilliseconds);
 };
