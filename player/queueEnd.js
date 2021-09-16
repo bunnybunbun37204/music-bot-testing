e3b585744cde2b091cbbdf43b3fa9e83386e5e4d
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2021-09-16 18:14:22 +0700

    remove waiting room

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,29 +1,3 @@
 module.exports = (client, message, queue) => {
-    const id = message.guild.me.voice.channel.id;
-    const channel = client.channels.cache.get(id.toString());
-    const delayTimeMs = 15 * 60000;
-    setTimeout(function () {
-        channel.join().then(connection => {
-            // Yay, it worked!
-            console.log("Successfully connected.");
-        }).catch(e => {
-            // Oh no, it errored! Let's log it to console :)
-            console.error(e);
-        });
-    }, 100);
-    console.log(message.guild.me.voice.channel.id);
-
-    if(!client.player.getQueue(message)) {
-        setTimeout(function () {
-            console.log(queue);
-            if(queue === [] || queue === null) {
-                const success = client.player.stop(message);
-            if(success) message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
-            }
-            else {
-                return;
-            }
-         }, delayTimeMs);
-    }
-
+    message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
 };
