29cbeb99586b46ec38c1f82ce06c65dc12f4dec7
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2021-09-28 23:40:17 +0700

    testing queue

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,3 +1,4 @@
 module.exports = (client, message, queue) => {
+    console.log(queue);
     message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
 };
