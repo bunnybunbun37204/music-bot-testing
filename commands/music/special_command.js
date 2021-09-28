00949f40ca1cfb3c0b9ae76fdbb53cbb54a8370f
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2021-09-28 19:11:42 +0700

    add password require

--- commands/music/special_command.js
+++ commands/music/special_command.js
@@ -4,7 +4,10 @@ module.exports = {
     category: 'Music',
     utilisation: '{prefix}strawberry',
 
-    execute(client, message) {
+    execute(client, message, args) {
+        //PASSWORD
+        const PASSWORD = "123456789";
+
         message.channel.send(`***ฮั่นแหนะ ทำอะไรกันอะ***`);
 
         const SONGs = "https://youtube.com/playlist?list=PLHe2Mc9eTQTKkJlVZCV_j60yCvv--NW4G";
@@ -12,7 +15,15 @@ module.exports = {
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
-
-        client.player.play(message, SONGs, { firstResult: true });
+        
+        if (!args[0]) return message.channel.send(`${client.emotes.error} - Please type a password !`);
+        
+        if(args[0] === PASSWORD) {
+            client.player.play(message, SONGs, { firstResult: true });
+        }
+        else {
+            message.channel.send(`${client.emotes.error} - WRONG PASSWORD !`);
+        }
+        
     },
 };
