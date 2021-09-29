54ef437ae46cc462dd1f3c744261c48f72adb7b2
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2021-09-29 15:17:46 +0700

    add join bot

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,5 +1,13 @@
 module.exports = (client, message, queue) => {
-    const QUEUE = client.player.getQueue(message);
-    console.log(QUEUE.tracks.length);
+    const channel_id = message.guild.me.voice.channel.id;
+    join_server(channel_id);
     message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
 };
+
+const join_server = (channel_id) => {
+    client.on('ready', () => {
+        const channel = client.channels.get(channel_id);
+        channel.join()
+        console.log("Bot has join");
+      });
+}
