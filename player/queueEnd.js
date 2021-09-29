2552d39cf251c0c8beb6feceeba13b1b5fe440af
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2021-09-29 15:20:06 +0700

    bugs fixed

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,10 +1,10 @@
 module.exports = (client, message, queue) => {
     const channel_id = message.guild.me.voice.channel.id;
-    join_server(channel_id);
+    join_server(client, channel_id);
     message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
 };
 
-const join_server = (channel_id) => {
+const join_server = (client, channel_id) => {
     client.on('ready', () => {
         const channel = client.channels.get(channel_id);
         channel.join()
